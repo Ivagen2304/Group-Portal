c57a073d38b48fd886d60e4f3cd08e5cfc16066e
Rounter228 <tivicity2@gmail.com>
2025-11-15 12:01:57 +0200

    Update Forum Models

--- Forum_app/views.py
+++ Forum_app/views.py
@@ -1,9 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import *
+from .models import Category, Topic, Message
 
 
 class CategoryListView(ListView):
     model = Category
     context_object_name = 'categoryes'
     template_name = 'Forum_app/categorylist.html'
+
+
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = 'topics'
+    template_name = 'Forum_app/topiclist.html'
+
+    def get_queryset(self):
+        category = get_object_or_404(Category, pk=self.kwargs['pk'])
+        return Topic.objects.filter(category=category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = get_object_or_404(Category, pk=self.kwargs['pk'])
+        return context
+
+
+class MessageListView(ListView):
+    model = Message
+    context_object_name = 'messages'
+    template_name = 'Forum_app/messagelist.html'
+
+    def get_queryset(self):
+        topic = get_object_or_404(Topic, pk=self.kwargs['pk'])
+        return Message.objects.filter(topic=topic)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['topic'] = get_object_or_404(Topic, pk=self.kwargs['pk'])
+        return context
