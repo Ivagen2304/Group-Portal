e887de55ef7cf02a678836f29fe8e2fd3bd12d77
Oksizm <ymutsic@gmail.com>
2025-11-26 18:54:38 +0200

    add crud

--- Portfolio_app/models.py
+++ Portfolio_app/models.py
@@ -5,6 +5,8 @@ class Portfolio(models.Model):
     name = models.CharField(max_length = 20)
     discription = models.TextField()
     creator = models.ForeignKey(User, on_delete = models.CASCADE)
+    link = models.CharField(max_length = 1000, blank = True, null = True)
+    file = models.FileField(upload_to = "portfoliofiles/", blank = True, null = True)
 
     def __str__(self):
         return self.name
@@ -12,6 +14,7 @@ class Portfolio(models.Model):
 class PortfolioImages(models.Model):
     portfolio = models.ForeignKey(Portfolio, on_delete = models.CASCADE)
     image = models.ImageField(upload_to = "portfolio/")
+
 
 
 # Create your models here.
