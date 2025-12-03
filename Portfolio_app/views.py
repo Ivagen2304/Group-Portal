8df2b69f8a9e320724820402ec862e2401c4ffaa
Oksizm <ymutsic@gmail.com>
2025-12-03 18:39:51 +0200

    add mixins

--- Portfolio_app/views.py
+++ Portfolio_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView, DetailView, DeleteView, CreateView, U
 from .models import Portfolio
 from .forms import PortfolioForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .mixins import UserIsOwnerMixin
 
 class PortfolioListView(ListView):
     model = Portfolio
@@ -25,16 +26,25 @@ class PortfolioCreate(LoginRequiredMixin, CreateView):
         form.instance.creator = self.request.user
         return super().form_valid(form)
     
-class PortfolioUpdate(UpdateView):
+class PortfolioUpdate(LoginRequiredMixin, UserIsOwnerMixin, UpdateView):
     model = Portfolio
     context_object_name = "portfolio"
     template_name = "Portfolio_app/portfolioupdate.html"
     form_class = PortfolioForm
     success_url = "/portfolio/"
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
     
-class PortfolioDelete(DeleteView):
+class PortfolioDelete(LoginRequiredMixin, UserIsOwnerMixin, DeleteView):
     model = Portfolio
     context_object_name = "portfolio"
     template_name = "Portfolio_app/portfoliodelete.html"
+    form_class = PortfolioForm
     success_url = "/portfolio/"
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
 # Create your views here.
